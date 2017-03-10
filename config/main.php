404eed7236df0d02993f8ea1129357ee93584930
Semenov <semenov@skeeks.com>
2017-03-10 21:36:52 +0300

    dev

---
+++ config/main.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright 2010 SkeekS
+ * @date 08.03.2017
+ */
+return
+[
+    'components' =>
+    [
+        /*'cmsBackend' =>
+        [
+            'class' => 'skeeks\cms\backend\BackendComponent'
+        ]*/
+    ]
+];
---
+++ src/Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright (c) 2010 SkeekS
+ * @date 10.03.2017
+ */
+namespace skeeks\cms\admin;
+/**
+ * Class Module
+ * @package skeeks\cms\admin
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'skeeks\cms\admin\controllers';
+}
