431d53a9288b29cfb3a824d357b5b8a2cccd12d5
semenov <semenov@skeeks.com>
2019-08-21 14:14:19 +0300

    dev

--- src/assets/src/js/classes/Window.js
+++ src/assets/src/js/classes/Window.js
@@ -2,71 +2,72 @@
  * @author Semenov Alexander <semenov@skeeks.com>
  * @link http://skeeks.com/
  * @copyright 2010 SkeekS (СкикС)
- * @date 28.02.2015
+ * @date 07.04.2015
  */
-(function(sx, $, _)
-{
+(function (sx, $, _) {
+
     sx.createNamespace('classes', sx);
-    /**
-     * @type {void|*|Function}
-     */
-    sx.classes.WindowOriginal  = sx.classes._Window.extend({});
 
     /**
-     * Настройка блокировщика для админки по умолчанию. Глобальное перекрытие
      * @type {void|*|Function}
      */
-    sx.classes.Window  = sx.classes._Window.extend({
+    sx.classes.Window = sx.classes._Window.extend({
 
         /**
          * @returns {Window}
          */
-        open: function()
-        {
+        open: function () {
             var self = this;
+
             this.trigger('beforeOpen');
             //строка параметров, собираем из массива
             var paramsSting = "";
-            if (this.getOpts())
-            {
-                _.each(this.getOpts(), function(value, key)
-                {
-                    if (paramsSting)
-                    {
+            if (this.getOpts()) {
+                _.each(this.getOpts(), function (value, key) {
+                    if (paramsSting) {
                         paramsSting = paramsSting + ',';
                     }
                     paramsSting = paramsSting + String(key) + "=" + String(value);
                 });
             }
 
-            this.onDomReady(function()
-            {
-                var options = _.extend({
-                    'afterClose' : function()
-                    {
-                        self.trigger('close');
-                    },
-                    'height'	: '100%',
-                    'autoSize'  : false,
-                    'width'		: '100%'
-                }, self.toArray());
+            var name = this.getName();
 
-                $("<a>", {
-                    'style' : 'display: none;',
-                    'href' : self._src,
-                    'data-fancybox-type' : 'iframe',
-                    'data-type' : 'iframe',
-                }).appendTo('body').fancybox(options).click();
+            this.onDomReady(function () {
+                $.fancybox.open({
+                    src: self._src,
+                    'type': 'iframe',
+                    'opts': {
+                        toolbar  : false,
+	                    smallBtn : true,
+                        /*buttons: [
+                            "close"
+                        ],*/
+
+                        'afterClose': function () {
+                            self.trigger('close');
+                        },
+                        iframe: {
+                            tpl: '<iframe id="fancybox-frame{rnd}" name="' + name + '" class="fancybox-iframe" allowfullscreen allow="autoplay; fullscreen" src=""></iframe>',
+                            preload: false,
+                            css: {
+                                width: '100%',
+                                height: '100%'
+                            }
+                        },
+                        animationDuration : 350,
+                        animationEffect   : 'slide-in-out',
+                    }
+                });
             });
 
             return this;
         },
 
-        close: function()
-        {
+        close: function() {
             $.fancybox.close();
-            return this;
         }
     });
+
 
 })(sx, sx.$, sx._);
